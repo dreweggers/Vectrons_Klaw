a2b4323e463377bade9e89ba1e4301e2820ed552
Steve Esswein <sjesswein1s@semo.edu>
2017-04-18 01:16:23 -0500

    Added a setup.py to the main directory.  This is used to initialize our program and give info on dependencies and such. Also began changing python files into definitions so that we can import them as modules, but I have only edited the bannergrab.py file so far.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Our initialization commands and all setup info will go in this file
+# This file is crucial to our entire project
+#  All important information will end up in the setup() function
+#
+# We should look at requirement files vs install_requires and see what is best for our dependency needs
+# Reference: http://packaging.python.org/distributing/
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+from net import __version__
+
+setup(name='Vectrons_Klaw',
+	version=__version__,
+	packages=['net'],
+	url='https://github.com/dreweggers/Vectrons_Klaw',
+	license='MIT',
+	author='Drew Eggers, Steve Esswein',
+	author_email='sjesswein1s@semo.edu',
+	description='Python Project for Network Testing',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Environment :: Console',
+		'Intended Audience :: Education',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: System :: Networking',
+	],
+	install_requires=[
+		'pcapy',
+	])
