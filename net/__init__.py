a18467c1dca0cb0f617701f04c038953e3327f6c
Steve Esswein <sjesswein1s@semo.edu>
2017-05-02 16:01:59 -0500

    Added vectrons_klaw as a  central testing point

--- net/__init__.py
+++ net/__init__.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 # We will add our module imports here in the future
 
+from collections import namedtuple
+
+__version_info__ = namedtuple(
+    'version_info',
+    'major minor micro releaselevel serial')(major=0,
+                                             minor=0,
+                                             micro=1,
+                                             releaselevel='alpha',
+                                             serial=0)
+
+__version__ = '{v.major}.{v.minor}.{v.micro}'.format(v=__version_info__)
---
+++ net/vectrons_klaw.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/python2
+# Vectrons_Klaw main file
+
+import findnw
+import bannergrab
+
+def main():
+	findnw.FindNw()
+	bannergrab.BannerGrab()
+
+if __name__ == '__main__':
+	main()
